df6bb72ad00bfb15629bbde00ab42e5626e844bd
Gökhan KESLER <g.kesler@nexum.com.tr>
2024-01-25 21:53:38 +0300

    Add project files.

---
+++ src/Presentation/Indivis.Presentation.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Indivis.Presentation.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
