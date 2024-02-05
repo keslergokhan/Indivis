936b528c0aab3bb9aa86f61576eaf486e58dbc7f
Gökhan KESLER <g.kesler@nexum.com.tr>
2024-02-05 22:24:33 +0300

    Fix

---
+++ src/Core/Indivis.Core.Application/Common/Dtos/BaseReadEntityDto.cs
@@ -0,0 +1,14 @@
+﻿using Indivis.Core.Application.Interfaces.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indivis.Core.Application.Common.Dtos
+{
+    public abstract class BaseReadEntityDto : IReadEntityDto
+    {
+        public Guid Id { get; set; }
+    }
+}
