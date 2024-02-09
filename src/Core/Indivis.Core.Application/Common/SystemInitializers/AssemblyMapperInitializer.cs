11e434990ea3a25ec4dc1a01bd36bfb2bb205780
Gökhan KESLER <g.kesler@nexum.com.tr>
2024-02-09 21:00:46 +0300

    Fix

--- src/Core/Indivis.Core.Application/Common/SystemInitializers/AssemblyMapperInitializer.cs
+++ src/Core/Indivis.Core.Application/Common/SystemInitializers/AssemblyMapperInitializer.cs
@@ -25,17 +25,27 @@ namespace Indivis.Core.Application.Common.SystemInitializers
         });
 
        
-
+        /// <summary>
+        /// Method içerisine gönderilen Assembly içerisinde CraeteMapAttribute tanımlanmış sınıflara ulaşır.
+        /// Sınıf üzerindeki tanımlanmış CreateMap gönderilmiş olan type değerlerini IMapperConfiguration içerisine ekler
+        /// 
+        /// </summary>
+        /// <param name="assembly"></param>
+        /// <param name="mapperConfiguration"></param>
         public void AssemblyCreateMapper(Assembly assembly, IMapperConfigurationExpression mapperConfiguration)
         {
+            //assembly içerisinde CreateMapAttribute tanımlanmış type değerlerini getir.
             assembly.GetTypes()?.Where(type => type.GetCustomAttribute<CreateMapAttribute>() is not null)?.ToList().ForEach(classType =>
             {
                 CreateMapAttribute attr = classType.GetCustomAttribute<CreateMapAttribute>();
 
                 if (attr.DestinationTypes.Length > 0)
                 {
+                    //içerisine tanımlanmış type değerlerini gez.
                     attr.DestinationTypes.ToList().ForEach(DTypeClass =>
                     {
+                        //çift taraflı tanımlama uygula
+                        //AObject -> BObject || BObject -> AObject
                         mapperConfiguration.CreateMap(classType, DTypeClass).ReverseMap();
                     });
                 }
