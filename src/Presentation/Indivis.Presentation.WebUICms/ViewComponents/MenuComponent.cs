22a24aa8da86ac0ef57aebfdfdd308bd0dc0093b
Gökhan KESLER <g.kesler@nexum.com.tr>
2024-07-21 16:58:42 +0300

    Fix

---
+++ src/Presentation/Indivis.Presentation.WebUICms/ViewComponents/MenuComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Indivis.Presentation.WebUICms.ViewComponents
+{
+	public class MenuComponent : ViewComponent
+	{
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			return View("~/Views/ViewComponents/MenuViewComponent.cshtml");
+		}
+	}
+}
